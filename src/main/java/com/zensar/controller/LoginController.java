701ae289451008427e1632581a88a5f5255a6048
Harshit <srivastavaharshit008@gmail.com>
2021-03-12 13:15:46 +0530

    first commit

---
+++ src/main/java/com/zensar/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.zensar.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+		
+	@PostMapping(value="/login/{username}/{password}")
+	public String login(@PathVariable("username")String uname,@PathVariable("password")String password) {
+		if(uname.equals("zen") && password.equals("zen"))
+		return "slogin";
+		else
+			return "flogin";
+	}
+}
